80334e1463576330088a4a548498b06f5a4b02c2
Benjamin Smith <bsmith@cabbage.org.uk>
2022-05-15 16:28:59 +0100

    Add first dependencies of Mojo::SQLite and IPC::System::Simple

--- bootstrap_perl5.sh
+++ bootstrap_perl5.sh
@@ -4,3 +4,7 @@ set -e -x
 
 perl bootstrap/cpanminus.pl -L $(pwd)/perl5 App::cpanminus local::lib
 
+. ./env.sh
+
+cpanm --installdeps .
+
---
+++ cpanfile
@@ -0,0 +1,4 @@
+requires 'perl', '5.034';
+
+requires 'Mojo::SQLite';
+requires 'IPC::System::Simple';
